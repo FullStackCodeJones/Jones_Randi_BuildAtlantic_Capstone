9d88d4abed6e1387d6f322e4cc004e8078bc2cc8
FullStackCodeJones <fullstackcodejones@gmail.com>
2025-01-08 14:03:52 -0500

    Added MONGO URI as well as server.js stage and gitignore

--- server.js
+++ server.js
@@ -1,1 +1,26 @@
 // Entry Point
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const connectDB = require("./config/db");
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Connect to MongoDB database
+connectDB();
+
+// Middleware configuration
+
+app.use(cors());
+app.use(express.json());
+
+//Routes
+
+app.get("/", (req, res) => res.send("Build Atlantic API is Running!"));
+
+//Starts The Server
+
+app.listen(PORT, () => console.log(`Server Running On Port ${PORT}`));
